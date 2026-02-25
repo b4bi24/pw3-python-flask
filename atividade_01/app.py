041f9e1785759da2c09ad3b973e30ba80f76a03f
BARBARA MEDINA FERREIRA <barbara.ferreira@e239.local>
2026-02-25 11:28:56 -0300

    atividade01 BARBARA

---
+++ atividade_01/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template 
+#render_template RENDERIZA AS PÁGINAS HTML
+
+
+app = Flask(__name__, template_folder='Views')
+
+@app.route('/')
+def home():
+    return render_template ("index.html")
+
+@app.route('/lista')
+def lista():
+    return render_template ("lista.html")
+
+@app.route('/formulario')
+def formulario():
+     return render_template ("formulario.html")
+
+# INICIANDO O SERVIDOR WEB
+if __name__ == '__main__':
+    app.run(debug=True)
